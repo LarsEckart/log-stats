be122218eb0cf07f5a71fc41c02e2855dddce103
Lars Eckart <lars.eckart@fortumo.com>
2017-04-22 12:10:47 +0300

    feature 6: parse the file content.
    Once the file was read, the content shall be passed to the parser.

---
+++ src/main/java/ee/larseckart/logstats/LogFileParser.java
@@ -0,0 +1,12 @@
+package ee.larseckart.logstats;
+
+import ee.larseckart.logstats.model.RequestInfo;
+
+import java.util.List;
+
+public class LogFileParser {
+
+    public List<RequestInfo> parse(String fileContent) {
+        throw new UnsupportedOperationException("parse() not implemented yet");
+    }
+}
--- src/test/java/ee/larseckart/logstats/LogStatsTest.java
+++ src/test/java/ee/larseckart/logstats/LogStatsTest.java
@@ -7,6 +7,7 @@ import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
 
 public class LogStatsTest {
@@ -19,6 +20,9 @@ public class LogStatsTest {
 
     @Mock
     private LogFileReader logFileReader;
+
+    @Mock
+    private LogFileParser logFileParser;
 
     private LogStats logStats;
 
@@ -108,5 +112,21 @@ public class LogStatsTest {
 
         // then
         verify(this.logFileReader).read("anyFileName");
+    }
+
+    @Test
+    public void should_pass_file_content_to_log_file_parser() throws Exception {
+        // given
+        final String[] args = new String[2];
+        args[0] = "anyFileName";
+        args[1] = "3";
+
+        given(this.logFileReader.read("anyFileName")).willReturn("anyFileContent");
+
+        // when
+        this.logStats.start(args);
+
+        // then
+        verify(this.logFileParser).parse("anyFileContent");
     }
 }
