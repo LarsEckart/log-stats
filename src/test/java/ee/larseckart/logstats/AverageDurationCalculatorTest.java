663ea213d74b0d9982a614268cbd659b4415bdc8
Lars Eckart <lars.eckart@fortumo.com>
2017-04-22 20:26:22 +0300

    feature 7: print top N avg duration resources
    test for the avg duration consumer.

--- src/test/java/ee/larseckart/logstats/AverageDurationCalculatorTest.java
+++ src/test/java/ee/larseckart/logstats/AverageDurationCalculatorTest.java
@@ -1,25 +1,71 @@
 package ee.larseckart.logstats;
 
-import ee.larseckart.logstats.model.RequestInfo;
+import ee.larseckart.logstats.model.TimedResource;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.BiConsumer;
+
+import static org.mockito.Mockito.verify;
 
 public class AverageDurationCalculatorTest {
 
+    @Rule
+    public MockitoRule mockitoRule = MockitoJUnit.rule();
+
+    @Mock
+    private Console console;
+
+    private BiConsumer<Integer, List<TimedResource>> averageDurationCalculator;
+
     @Before
     public void initialize() throws Exception {
-
+        this.averageDurationCalculator = new AverageDurationCalculator();
     }
 
     @Test
     public void should_print_n_resources_who_have_highest_avg_duration() throws Exception {
         // given
-
+        final List<TimedResource> timedResources = Arrays.asList(
+                new AnyTimedResource("A", 2L), new AnyTimedResource("B", 4L),
+                new AnyTimedResource("A", 8L), new AnyTimedResource("B", 16L),
+                new AnyTimedResource("C", 3L), new AnyTimedResource("C", 9L),
+                new AnyTimedResource("D", 1L), new AnyTimedResource("D", 1L));
+        // A: 5 ; B: 10 ; C: 6 ; D: 1
 
         // when
+        this.averageDurationCalculator.accept(2, timedResources);
 
         // then
+        verify(this.console).printLine("B: 10");
+        verify(this.console).printLine("C: 6");
+    }
+
+    class AnyTimedResource implements TimedResource {
+
+        private final String resource;
+        private final long duration;
+
+        public AnyTimedResource(String resource, long duration) {
+
+            this.resource = resource;
+            this.duration = duration;
+        }
+
+        @Override
+        public String getResource() {
+            return this.resource;
+        }
+
+        @Override
+        public long getDuration() {
+            return this.duration;
+        }
     }
 }
