e1d27a0a708d7d09187d60a1d3a5ee4ffb2f9304
Lars Eckart <lars.eckart@fortumo.com>
2017-04-22 16:34:02 +0300

    feature 6: parse the file content.
    refactor

--- src/main/java/ee/larseckart/logstats/LogFileLineParser.java
+++ src/main/java/ee/larseckart/logstats/LogFileLineParser.java
@@ -10,37 +10,47 @@ import java.util.regex.Pattern;
 
 public class LogFileLineParser implements Function<String, RequestInfo> {
 
+    private static final String WHITESPACE = "\\s";
+    private static final Pattern SPLIT_ARGUMENT = Pattern.compile(WHITESPACE);
+
     @Override
     public RequestInfo apply(String text) {
-        final Pattern splitArgument = Pattern.compile(" ");
-        final String[] lineItems = text.split(splitArgument.pattern());
+        final String[] lineItems = text.split(SPLIT_ARGUMENT.pattern());
 
         final RequestInfo.Builder builder = new RequestInfo.Builder();
 
-        LocalDate date = LocalDate.parse(lineItems[0]);
-        builder.date(date);
-
-        DateTimeFormatter format = DateTimeFormatter.ofPattern("HH:mm:ss,SSS");
-        LocalTime time = LocalTime.parse(lineItems[1], format);
-        builder.timestamp(time);
-
-        final String rawThreadId = lineItems[2];
-        final String threadId = rawThreadId.substring(1, rawThreadId.length() - 1);
-        builder.threadId(threadId);
-
-        final String rawUserContext = lineItems[3];
-        String userContext = rawUserContext.substring(1, rawUserContext.length() - 1);
-        builder.userContext(userContext);
-
+        parseDate(lineItems[0], builder);
+        parseTimestamp(lineItems[1], builder);
+        parseThreadId(lineItems[2], builder);
+        parseUserContext(lineItems[3], builder);
         parseResource(lineItems[4], builder);
-
-        for (int i = 5; !("in".equals(lineItems[i])); i++) {
-            builder.payloadElement(lineItems[i]);
-        }
-
-        builder.duration(Long.parseLong(lineItems[lineItems.length - 1]));
+        parsePayloadElements(lineItems, builder);
+        parseDuration(lineItems[lineItems.length - 1], builder);
 
         return builder.build();
+    }
+
+    private void parseDate(String lineItem, RequestInfo.Builder builder) {
+        LocalDate date = LocalDate.parse(lineItem);
+        builder.date(date);
+    }
+
+    private void parseTimestamp(String lineItem, RequestInfo.Builder builder) {
+        DateTimeFormatter format = DateTimeFormatter.ofPattern("HH:mm:ss,SSS");
+        LocalTime time = LocalTime.parse(lineItem, format);
+        builder.timestamp(time);
+    }
+
+    private void parseThreadId(String lineItem, RequestInfo.Builder builder) {
+        final String rawThreadId = lineItem;
+        final String threadId = rawThreadId.substring(1, rawThreadId.length() - 1);
+        builder.threadId(threadId);
+    }
+
+    private void parseUserContext(String lineItem, RequestInfo.Builder builder) {
+        final String rawUserContext = lineItem;
+        String userContext = rawUserContext.substring(1, rawUserContext.length() - 1);
+        builder.userContext(userContext);
     }
 
     private void parseResource(String rawResource, RequestInfo.Builder builder) {
@@ -58,6 +68,10 @@ public class LogFileLineParser implements Function<String, RequestInfo> {
         }
     }
 
+    private boolean isUriWithQuery(String rawResource) {
+        return rawResource.startsWith("/") && rawResource.contains("?");
+    }
+
     // TODO: complicated piece of code
     private int determineEndOfAction(String rawResource) {
         final int actionKeyStart = rawResource.indexOf("action=");
@@ -67,7 +81,13 @@ public class LogFileLineParser implements Function<String, RequestInfo> {
         return actionStartIndex + actionEndIndex;
     }
 
-    private boolean isUriWithQuery(String rawResource) {
-        return rawResource.startsWith("/") && rawResource.contains("?");
+    private void parsePayloadElements(String[] lineItems, RequestInfo.Builder builder) {
+        for (int i = 5; !("in".equals(lineItems[i])); i++) {
+            builder.payloadElement(lineItems[i]);
+        }
+    }
+
+    private void parseDuration(String lineItem, RequestInfo.Builder builder) {
+        builder.duration(Long.parseLong(lineItem));
     }
 }
