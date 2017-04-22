3314a06be42260d57f73e4edefadcc03de49ab23
Lars Eckart <lars.eckart@fortumo.com>
2017-04-22 16:59:25 +0300

    feature 6: parse the file content.
    refactor, remove magic numbers, clean up query parsing method

--- src/main/java/ee/larseckart/logstats/LogFileLineParser.java
+++ src/main/java/ee/larseckart/logstats/LogFileLineParser.java
@@ -10,6 +10,13 @@ import java.util.function.Function;
 public class LogFileLineParser implements Function<String, RequestInfo> {
 
     private static final String LINE_ITEM_SPLIT_ARGUMENT = " ";
+    private static final int DATE_INDEX = 0;
+    private static final int TIMESTAMP_INDEX = 1;
+    private static final int THREAD_ID_INDEX = 2;
+    private static final int USER_CONTEXT_INDEX = 3;
+    private static final int RESOURCE_INDEX = 4;
+    private static final int PAYLOAD_ELEMENTS_INDEX = 5;
+    private static final String ACTION_QUERY_PARAM = "action=";
 
     @Override
     public RequestInfo apply(String text) {
@@ -17,19 +24,20 @@ public class LogFileLineParser implements Function<String, RequestInfo> {
 
         final RequestInfo.Builder builder = new RequestInfo.Builder();
 
-        parseDate(lineItems[0], builder);
-        parseTimestamp(lineItems[1], builder);
-        parseThreadId(lineItems[2], builder);
-        parseUserContext(lineItems[3], builder);
-        parseResource(lineItems[4], builder);
+        parseDate(lineItems[DATE_INDEX], builder);
+        parseTimestamp(lineItems[TIMESTAMP_INDEX], builder);
+        parseThreadId(lineItems[THREAD_ID_INDEX], builder);
+        parseUserContext(lineItems[USER_CONTEXT_INDEX], builder);
+        parseResource(lineItems[RESOURCE_INDEX], builder);
         parsePayloadElements(lineItems, builder);
-        parseDuration(lineItems[lineItems.length - 1], builder);
+        int durationIndex = lineItems.length - 1;
+        parseDuration(lineItems[durationIndex], builder);
 
         return builder.build();
     }
 
     private void parseDate(String lineItem, RequestInfo.Builder builder) {
-        LocalDate date = LocalDate.parse(lineItem);
+        LocalDate date = LocalDate.parse(lineItem, DateTimeFormatter.ISO_LOCAL_DATE);
         builder.date(date);
     }
 
@@ -40,27 +48,22 @@ public class LogFileLineParser implements Function<String, RequestInfo> {
     }
 
     private void parseThreadId(String lineItem, RequestInfo.Builder builder) {
-        final String rawThreadId = lineItem;
-        final String threadId = rawThreadId.substring(1, rawThreadId.length() - 1);
+        final String threadId = removeFirstAndLastCharacter(lineItem);
         builder.threadId(threadId);
     }
 
     private void parseUserContext(String lineItem, RequestInfo.Builder builder) {
-        final String rawUserContext = lineItem;
-        String userContext = rawUserContext.substring(1, rawUserContext.length() - 1);
+        String userContext = removeFirstAndLastCharacter(lineItem);
         builder.userContext(userContext);
+    }
+
+    private String removeFirstAndLastCharacter(String text) {
+        return text.substring(1, text.length() - 1);
     }
 
     private void parseResource(String rawResource, RequestInfo.Builder builder) {
         if (isUriWithQuery(rawResource)) {
-            if (rawResource.contains("?action=")) {
-                int endOfAction = determineEndOfAction(rawResource);
-                final String uriWithAction = rawResource.substring(0, endOfAction);
-                builder.resource(uriWithAction);
-            } else {
-                final String uriWithoutQuery = rawResource.substring(0, rawResource.indexOf("?"));
-                builder.resource(uriWithoutQuery);
-            }
+            parseUriWithQuery(rawResource, builder);
         } else {
             builder.resource(rawResource);
         }
@@ -70,17 +73,39 @@ public class LogFileLineParser implements Function<String, RequestInfo> {
         return rawResource.startsWith("/") && rawResource.contains("?");
     }
 
-    // TODO: complicated piece of code
-    private int determineEndOfAction(String rawResource) {
-        final int actionKeyStart = rawResource.indexOf("action=");
-        int actionStartIndex = actionKeyStart + 7;
-        final String queryStringAfterAction = rawResource.substring(actionStartIndex, rawResource.length() - 1);
-        final int actionEndIndex = queryStringAfterAction.indexOf("&");
-        return actionStartIndex + actionEndIndex;
+    private void parseUriWithQuery(String rawResource, RequestInfo.Builder builder) {
+        if (isUriWithActionQuery(rawResource)) {
+            parseUriWithActionQuery(rawResource, builder);
+        } else {
+            final String uriWithoutQuery = rawResource.substring(0, rawResource.indexOf("?"));
+            builder.resource(uriWithoutQuery);
+        }
+    }
+
+    private boolean isUriWithActionQuery(String rawResource) {
+        return rawResource.contains(ACTION_QUERY_PARAM);
+    }
+
+    private void parseUriWithActionQuery(String rawResource, RequestInfo.Builder builder) {
+        int endOfActionIndex = calculateEndOfAction(rawResource);
+        final String uriWithAction = rawResource.substring(0, endOfActionIndex);
+        builder.resource(uriWithAction);
+    }
+
+    /** Assumes that action is always the first query parameter! */
+    private int calculateEndOfAction(String rawResource) {
+        final int actionKeyStartIndex = rawResource.indexOf(ACTION_QUERY_PARAM);
+        int offset = ACTION_QUERY_PARAM.length();
+        int actionValueStartIndex = actionKeyStartIndex + offset;
+
+        final String uriAfterActionQueryKey = rawResource.substring(actionValueStartIndex, rawResource.length() - 1);
+        final int actionValueEndIndex = uriAfterActionQueryKey.indexOf("&");
+
+        return actionValueStartIndex + actionValueEndIndex;
     }
 
     private void parsePayloadElements(String[] lineItems, RequestInfo.Builder builder) {
-        for (int i = 5; !("in".equals(lineItems[i])); i++) {
+        for (int i = PAYLOAD_ELEMENTS_INDEX; !("in".equals(lineItems[i])); i++) {
             builder.payloadElement(lineItems[i]);
         }
     }
