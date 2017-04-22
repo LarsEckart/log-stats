970367ddd83a8b87719bfc99e127a2450b788016
Lars Eckart <lars.eckart@fortumo.com>
2017-04-22 15:02:51 +0300

    feature 6: parse the file content.
    split file lines and parse them to request info objects

--- src/main/java/ee/larseckart/logstats/LogFileParser.java
+++ src/main/java/ee/larseckart/logstats/LogFileParser.java
@@ -4,13 +4,19 @@ import ee.larseckart.logstats.model.RequestInfo;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class LogFileParser {
 
     public List<RequestInfo> parse(String fileContent) {
         List<RequestInfo> result = new ArrayList<>();
-        final RequestInfo.Builder builder = new RequestInfo.Builder();
-        result.add(builder.build());
+
+        final long count = Pattern.compile("\n").splitAsStream(fileContent).count();
+
+        for (int i = 0; i < count; i++) {
+            final RequestInfo.Builder builder = new RequestInfo.Builder();
+            result.add(builder.build());
+        }
         return result;
     }
 }
