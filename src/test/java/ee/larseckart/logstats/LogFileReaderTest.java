fe909c6be3338f1185673d566b39d9268a6ee47e
Lars Eckart <lars.eckart@fortumo.com>
2017-04-22 14:57:13 +0300

    feature 6: parse the file content.
    back from baby break, refactor tests

--- src/test/java/ee/larseckart/logstats/LogFileReaderTest.java
+++ src/test/java/ee/larseckart/logstats/LogFileReaderTest.java
@@ -7,6 +7,7 @@ import org.junit.rules.TemporaryFolder;
 
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
@@ -14,6 +15,8 @@ import java.util.List;
 import static org.assertj.core.api.Java6Assertions.assertThat;
 
 public class LogFileReaderTest {
+
+    private static final String ANY_FILE_NAME = "any_file_name";
 
     @Rule
     public TemporaryFolder temporaryFolder = new TemporaryFolder();
@@ -30,18 +33,19 @@ public class LogFileReaderTest {
     @Test
     public void should_read_file_and_return_content() throws Exception {
         // given
-        String tempFilePath = this.temporaryFolder.getRoot() + "/anyFile";
+        String filePath = this.temporaryFolder.getRoot() + "/" + ANY_FILE_NAME;
 
         // when
-        final String fileContent = this.logFileReader.read(tempFilePath);
+        String fileContent = this.logFileReader.read(filePath);
 
         // then
         assertThat(fileContent).isEqualTo("line1\nline2\n");
     }
 
     private void initializeTempFile() throws IOException {
-        this.temporaryFolder.newFile("anyFile");
+        this.temporaryFolder.newFile(ANY_FILE_NAME);
         List<String> lines = Arrays.asList("line1", "line2");
-        Files.write(Paths.get(this.temporaryFolder.getRoot() + "/anyFile"), lines);
+        Path filePath = Paths.get(this.temporaryFolder.getRoot() + "/" + ANY_FILE_NAME);
+        Files.write(filePath, lines);
     }
 }
--- src/test/java/ee/larseckart/logstats/LogStatsTest.java
+++ src/test/java/ee/larseckart/logstats/LogStatsTest.java
@@ -12,6 +12,12 @@ import static org.mockito.Mockito.verify;
 
 public class LogStatsTest {
 
+    private static final String ANY_ARGS = "anyArgs";
+    private static final String ANY_FILE_NAME = "any_file_name";
+    private static final String ANY_FILE_CONTENT = "any_file_content";
+    private static final String NOT_A_NUMBER = "NaN";
+    private static final String ANY_NUMBER = "3";
+
     @Rule
     public MockitoRule mockitoRule = MockitoJUnit.rule();
 
@@ -42,7 +48,7 @@ public class LogStatsTest {
         // then
         verify(this.console).printLine("No args provided, run with -h flag for help.");
     }
-    
+
     @Test
     public void should_print_how_to_use_the_application_when_args_with_help_flag() throws Exception {
         // given
@@ -59,10 +65,10 @@ public class LogStatsTest {
     }
 
     @Test
-    public void should_print_how_to_use_the_application_when_args_but_not_help_flag() throws Exception {
+    public void should_print_how_to_use_the_application_when_one_argument_but_not_help_flag() throws Exception {
         // given
         final String[] args = new String[1];
-        args[0] = "anyArgs";
+        args[0] = ANY_ARGS;
 
         // when
         this.logStats.start(args);
@@ -75,9 +81,9 @@ public class LogStatsTest {
     public void should_print_how_to_use_the_application_when_more_than_2_args() throws Exception {
         // given
         final String[] args = new String[3];
-        args[0] = "anyArgs";
-        args[1] = "anyArgs";
-        args[2] = "anyArgs";
+        args[0] = ANY_ARGS;
+        args[1] = ANY_ARGS;
+        args[2] = ANY_ARGS;
 
         // when
         this.logStats.start(args);
@@ -90,8 +96,8 @@ public class LogStatsTest {
     public void should_print_how_to_use_the_application_when_2_args_but_second_arg_is_no_number() throws Exception {
         // given
         final String[] args = new String[2];
-        args[0] = "anyArgs";
-        args[1] = "NaN";
+        args[0] = ANY_ARGS;
+        args[1] = NOT_A_NUMBER;
 
         // when
         this.logStats.start(args);
@@ -104,29 +110,29 @@ public class LogStatsTest {
     public void should_pass_first_argument_to_logfile_reader_when_correct_arguments() throws Exception {
         // given
         final String[] args = new String[2];
-        args[0] = "anyFileName";
-        args[1] = "3";
+        args[0] = ANY_FILE_NAME;
+        args[1] = ANY_NUMBER;
 
         // when
         this.logStats.start(args);
 
         // then
-        verify(this.logFileReader).read("anyFileName");
+        verify(this.logFileReader).read(ANY_FILE_NAME);
     }
 
     @Test
     public void should_pass_file_content_to_log_file_parser() throws Exception {
         // given
         final String[] args = new String[2];
-        args[0] = "anyFileName";
-        args[1] = "3";
+        args[0] = ANY_FILE_NAME;
+        args[1] = ANY_NUMBER;
 
-        given(this.logFileReader.read("anyFileName")).willReturn("anyFileContent");
+        given(this.logFileReader.read(ANY_FILE_NAME)).willReturn(ANY_FILE_CONTENT);
 
         // when
         this.logStats.start(args);
 
         // then
-        verify(this.logFileParser).parse("anyFileContent");
+        verify(this.logFileParser).parse(ANY_FILE_CONTENT);
     }
 }
