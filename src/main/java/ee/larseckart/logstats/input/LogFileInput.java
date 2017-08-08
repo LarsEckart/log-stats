613d1f17f8235b5433b5688807b14b78ded28cb3
Lars Eckart <lars.eckart@fortumo.com>
2017-08-08 21:21:30 +0300

    ensure stream is (auto-)closed when reading file

--- src/main/java/ee/larseckart/logstats/input/LogFileInput.java
+++ src/main/java/ee/larseckart/logstats/input/LogFileInput.java
@@ -2,7 +2,6 @@ package ee.larseckart.logstats.input;
 
 import ee.larseckart.logstats.model.TimedResource;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
@@ -10,6 +9,7 @@ import java.nio.file.Paths;
 import java.util.List;
 import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class LogFileInput implements Function<String, List<TimedResource>> {
 
@@ -21,11 +21,12 @@ public class LogFileInput implements Function<String, List<TimedResource>> {
 
     @Override
     public List<TimedResource> apply(String fileName) {
-        try {
-            return Files.lines(Paths.get(fileName), StandardCharsets.UTF_8)
-                        .map(line -> this.logFileLineParser.apply(line))
-                        .collect(Collectors.toList());
-        } catch (IOException exception) {
+        try (Stream<String> stream = Files.lines(Paths.get(fileName), StandardCharsets.UTF_8)) {
+            return stream.map(line -> this.logFileLineParser.apply(line))
+                         .collect(Collectors.toList());
+        } catch (
+                IOException exception)
+        {
             throw new IllegalArgumentException("Could not read file '" + fileName + "'.", exception);
         }
     }
