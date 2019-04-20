9958ca7ddbe576f5e6cea39604b7e0389de80a2b
Lars Eckart <lars.eckart@gmail.com>
2019-04-20 09:19:14 +0300

    reset to zero

--- src/main/java/ee/larseckart/logstats/Main.java
+++ src/main/java/ee/larseckart/logstats/Main.java
@@ -1,13 +1,8 @@
 package ee.larseckart.logstats;
 
-import io.micronaut.context.ApplicationContext;
-
 public class Main {
 
     public static void main(String[] args) {
-        try (ApplicationContext context = ApplicationContext.run()) {
-            LogStats myBean = context.getBean(LogStats.class);
-            myBean.execute(args);
-        }
+
     }
 }
