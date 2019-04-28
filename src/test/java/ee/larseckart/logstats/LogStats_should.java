69626046366cfbbb41def7f903cf48c4116dffa8
Lars Eckart <lars.eckart@gmail.com>
2019-04-28 19:39:08 +0300

    migrate to junit5's tempDir feature

--- src/test/java/ee/larseckart/logstats/LogStats_should.java
+++ src/test/java/ee/larseckart/logstats/LogStats_should.java
@@ -1,13 +1,17 @@
 package ee.larseckart.logstats;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.PrintStream;
+import java.io.UnsupportedEncodingException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -94,112 +98,125 @@ class LogStats_should {
         );
     }
 
-    @Test
-    void prints_resource_and_request_time_when_1_entry_in_file() throws Exception {
-        // given
-        File tempFile = File.createTempFile("any", ".log");
-        var lines = List.of("2015-08-19 00:00:01,049 (http--0.0.0.0-28080-405) [] /checkSession.do in 100");
-        Files.write(tempFile.toPath(), lines);
-        String[] argumentWithFileAndNumberArgument = {tempFile.getAbsolutePath(), "1"};
+    @Nested
+    class AverageRequestTimes {
 
-        // when
-        logStats.run(argumentWithFileAndNumberArgument);
+        @BeforeEach
+        void setUp() throws UnsupportedEncodingException {
+            out = new ByteArrayOutputStream();
+            PrintStream printStream = new PrintStream(out, true, StandardCharsets.UTF_8.name());
+            Console console = new Console(printStream);
+            logStats = new LogStatsImpl(console, new AverageRequestTime(console));
+        }
 
-        // then
-        assertThat(out.toString()).endsWith("\n/checkSession.do 100.0");
-    }
+        @Test
+        void prints_resource_and_request_time_when_1_entry_in_file(@TempDir Path tempDir) throws Exception {
+            // given
+            var tempFile = tempDir.resolve("any_log_file.log");
+            var lines = List.of("2015-08-19 00:00:01,049 (http--0.0.0.0-28080-405) [] /checkSession.do in 100");
+            Files.write(tempFile, lines);
+            String[] argumentWithFileAndNumberArgument = {tempFile.toString(), "1"};
 
-    @Test
-    void prints_resource_and_avg_request_time_when_2_entries_in_file() throws Exception {
-        // given
-        File tempFile = File.createTempFile("any", ".log");
-        var lines = List.of("2015-08-19 00:00:01,049 (http--0.0.0.0-28080-405) [] /checkSession.do in 100",
-                "2015-08-19 00:00:01,049 (http--0.0.0.0-28080-405) [] /checkSession.do in 200");
-        Files.write(tempFile.toPath(), lines);
-        String[] argumentWithFileAndNumberArgument = {tempFile.getAbsolutePath(), "1"};
+            // when
+            logStats.run(argumentWithFileAndNumberArgument);
 
-        // when
-        logStats.run(argumentWithFileAndNumberArgument);
+            // then
+            assertThat(out.toString()).endsWith("\n/checkSession.do 100.0");
+        }
 
-        // then
-        assertThat(out.toString()).endsWith("\n/checkSession.do 150.0");
-    }
+        @Test
+        void prints_resource_and_avg_request_time_when_2_entries_in_file(@TempDir Path tempDir) throws Exception {
+            // given
+            var tempFile = tempDir.resolve("any_log_file.log");
+            var lines = List.of("2015-08-19 00:00:01,049 (http--0.0.0.0-28080-405) [] /checkSession.do in 100",
+                    "2015-08-19 00:00:01,049 (http--0.0.0.0-28080-405) [] /checkSession.do in 200");
+            Files.write(tempFile, lines);
+            String[] argumentWithFileAndNumberArgument = {tempFile.toString(), "1"};
 
-    @Test
-    void prints_resource_and_avg_request_time_ordered_by_avg_request_time() throws Exception {
-        // given
-        File tempFile = File.createTempFile("any", ".log");
-        var lines = List.of(
-                "2015-08-19 00:00:01,049 (http--0.0.0.0-28080-405) [] /checkSession.do in 100",
-                "2015-08-19 00:00:01,049 (http--0.0.0.0-28080-405) [] getBroadbandSubscriptions in 500",
-                "2015-08-19 00:00:01,049 (http--0.0.0.0-28080-405) [] /checkSession.do in 100",
-                "2015-08-19 00:00:01,049 (http--0.0.0.0-28080-405) [] getBroadbandSubscriptions in 500",
-                "2015-08-19 00:00:01,049 (http--0.0.0.0-28080-405) [] /checkSession.do in 100",
-                "2015-08-19 00:00:01,049 (http--0.0.0.0-28080-405) [] getBroadbandSubscriptions in 500");
-        Files.write(tempFile.toPath(), lines);
-        String[] argumentWithFileAndNumberArgument = {tempFile.getAbsolutePath(), "2"};
+            // when
+            logStats.run(argumentWithFileAndNumberArgument);
 
-        // when
-        logStats.run(argumentWithFileAndNumberArgument);
+            // then
+            assertThat(out.toString()).endsWith("\n/checkSession.do 150.0");
+        }
 
-        // then
-        assertThat(out.toString()).endsWith("\ngetBroadbandSubscriptions 500.0\n/checkSession.do 100.0");
-    }
+        @Test
+        void prints_resource_and_avg_request_time_ordered_by_avg_request_time(@TempDir Path tempDir) throws Exception {
+            // given
+            var tempFile = tempDir.resolve("any_log_file.log");
+            var lines = List.of(
+                    "2015-08-19 00:00:01,049 (http--0.0.0.0-28080-405) [] /checkSession.do in 100",
+                    "2015-08-19 00:00:01,049 (http--0.0.0.0-28080-405) [] getBroadbandSubscriptions in 500",
+                    "2015-08-19 00:00:01,049 (http--0.0.0.0-28080-405) [] /checkSession.do in 100",
+                    "2015-08-19 00:00:01,049 (http--0.0.0.0-28080-405) [] getBroadbandSubscriptions in 500",
+                    "2015-08-19 00:00:01,049 (http--0.0.0.0-28080-405) [] /checkSession.do in 100",
+                    "2015-08-19 00:00:01,049 (http--0.0.0.0-28080-405) [] getBroadbandSubscriptions in 500");
+            Files.write(tempFile, lines);
+            String[] argumentWithFileAndNumberArgument = {tempFile.toString(), "2"};
 
-    @Test
-    void prints_resource_and_avg_request_time_ordered_by_avg_request_time_and_adheres_to_provided_limit() throws Exception {
-        // given
-        File tempFile = File.createTempFile("any", ".log");
-        var lines = List.of(
-                "2015-08-19 00:00:01,049 (http--0.0.0.0-28080-405) [] /checkSession.do in 100",
-                "2015-08-19 00:00:01,049 (http--0.0.0.0-28080-405) [] getBroadbandSubscriptions in 500",
-                "2015-08-19 00:00:01,049 (http--0.0.0.0-28080-405) [] /checkSession.do in 100",
-                "2015-08-19 00:00:01,049 (http--0.0.0.0-28080-405) [] getBroadbandSubscriptions in 500",
-                "2015-08-19 00:00:01,049 (http--0.0.0.0-28080-405) [] /checkSession.do in 100",
-                "2015-08-19 00:00:01,049 (http--0.0.0.0-28080-405) [] getBroadbandSubscriptions in 500");
-        Files.write(tempFile.toPath(), lines);
-        String[] argumentWithFileAndNumberArgument = {tempFile.getAbsolutePath(), "1"};
+            // when
+            logStats.run(argumentWithFileAndNumberArgument);
 
-        // when
-        logStats.run(argumentWithFileAndNumberArgument);
+            // then
+            assertThat(out.toString()).endsWith("\ngetBroadbandSubscriptions 500.0\n/checkSession.do 100.0");
+        }
 
-        // then
-        assertThat(out.toString()).endsWith("\ngetBroadbandSubscriptions 500.0");
-    }
+        @Test
+        void prints_resource_and_avg_request_time_ordered_by_avg_request_time_and_adheres_to_provided_limit(@TempDir Path tempDir)
+                throws Exception {
+            // given
+            var tempFile = tempDir.resolve("any_log_file.log");
+            var lines = List.of(
+                    "2015-08-19 00:00:01,049 (http--0.0.0.0-28080-405) [] /checkSession.do in 100",
+                    "2015-08-19 00:00:01,049 (http--0.0.0.0-28080-405) [] getBroadbandSubscriptions in 500",
+                    "2015-08-19 00:00:01,049 (http--0.0.0.0-28080-405) [] /checkSession.do in 100",
+                    "2015-08-19 00:00:01,049 (http--0.0.0.0-28080-405) [] getBroadbandSubscriptions in 500",
+                    "2015-08-19 00:00:01,049 (http--0.0.0.0-28080-405) [] /checkSession.do in 100",
+                    "2015-08-19 00:00:01,049 (http--0.0.0.0-28080-405) [] getBroadbandSubscriptions in 500");
+            Files.write(tempFile, lines);
+            String[] argumentWithFileAndNumberArgument = {tempFile.toString(), "1"};
 
-    @Test
-    void ignores_input_lines_that_do_not_have_valid_request_time() throws Exception {
-        // given
-        File tempFile = File.createTempFile("any", ".log");
-        var lines = List.of(
-                "2015-08-19 00:00:01,049 (http--0.0.0.0-28080-405) [] /checkSession.do in 100",
-                "2016-01-19 20:27:08,928 (http--0.0.0.0-28080-16) [USER:358405537695] /mainContent.do?action=CAROUSEL in {}",
-                "2015-08-19 00:00:01,049 (http--0.0.0.0-28080-405) [] /checkSession.do in 100",
-                "2015-08-19 00:00:01,049 (http--0.0.0.0-28080-405) [] /checkSession.do in 100");
-        Files.write(tempFile.toPath(), lines);
-        String[] argumentWithFileAndNumberArgument = {tempFile.getAbsolutePath(), "1"};
+            // when
+            logStats.run(argumentWithFileAndNumberArgument);
 
-        // when
-        logStats.run(argumentWithFileAndNumberArgument);
+            // then
+            assertThat(out.toString()).endsWith("\ngetBroadbandSubscriptions 500.0");
+        }
 
-        // then
-        assertThat(out.toString()).endsWith("\n/checkSession.do 100.0");
-    }
+        @Test
+        void ignores_input_lines_that_do_not_have_valid_request_time(@TempDir Path tempDir) throws Exception {
+            // given
+            var tempFile = tempDir.resolve("any_log_file.log");
+            var lines = List.of(
+                    "2015-08-19 00:00:01,049 (http--0.0.0.0-28080-405) [] /checkSession.do in 100",
+                    "2016-01-19 20:27:08,928 (http--0.0.0.0-28080-16) [USER:358405537695] /mainContent.do?action=CAROUSEL in {}",
+                    "2015-08-19 00:00:01,049 (http--0.0.0.0-28080-405) [] /checkSession.do in 100",
+                    "2015-08-19 00:00:01,049 (http--0.0.0.0-28080-405) [] /checkSession.do in 100");
+            Files.write(tempFile, lines);
+            String[] argumentWithFileAndNumberArgument = {tempFile.toString(), "1"};
 
-    @Test
-    void print_info_about_bad_entries() throws Exception {
-        // given
-        File tempFile = File.createTempFile("any", ".log");
-        var lines = List.of(
-                "2016-01-19 20:27:08,928 (http--0.0.0.0-28080-16) [USER:358405537695] /mainContent.do?action=CAROUSEL in {}");
-        Files.write(tempFile.toPath(), lines);
-        String[] argumentWithFileAndNumberArgument = {tempFile.getAbsolutePath(), "1"};
+            // when
+            logStats.run(argumentWithFileAndNumberArgument);
 
-        // when
-        logStats.run(argumentWithFileAndNumberArgument);
+            // then
+            assertThat(out.toString()).endsWith("\n/checkSession.do 100.0");
+        }
 
-        // then
-        assertThat(out.toString()).contains(
-                "\nInvalid line: 2016-01-19 20:27:08,928 (http--0.0.0.0-28080-16) [USER:358405537695] /mainContent.do?action=CAROUSEL in {}");
+        @Test
+        void print_info_about_bad_entries(@TempDir Path tempDir) throws Exception {
+            // given
+            var tempFile = tempDir.resolve("any_log_file.log");
+            var lines = List.of(
+                    "2016-01-19 20:27:08,928 (http--0.0.0.0-28080-16) [USER:358405537695] /mainContent.do?action=CAROUSEL in {}");
+            Files.write(tempFile, lines);
+            String[] argumentWithFileAndNumberArgument = {tempFile.toString(), "1"};
+
+            // when
+            logStats.run(argumentWithFileAndNumberArgument);
+
+            // then
+            assertThat(out.toString()).contains(
+                    "\nInvalid line: 2016-01-19 20:27:08,928 (http--0.0.0.0-28080-16) [USER:358405537695] /mainContent.do?action=CAROUSEL in {}");
+        }
     }
 }
